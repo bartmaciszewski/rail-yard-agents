b709b778408a682b918973c95af29ee3d9b15380
Bart <Bart@DSVMDEV01.(None)>
2019-01-14 05:12:49 +0000

    Single track single loco simple RL model

--- Railyard_simulator.py
+++ Railyard_simulator.py
@@ -6,7 +6,8 @@ import tensorflow as tf
 
 BACKWARD = 0
 FORWARD = 1
-BATCH_SIZE = 200
+NUM_EPOCHS = 500 #Each epoch is a training iteration and 2500 essentially optimizes the network
+NUM_EPISODES = 1
 
 class Truck:
     inventory = 0
@@ -32,11 +33,17 @@ class Locomotive:
     def move(self, action):
         self.current_track[self.current_position] = None
         #Backward
-        if action == BACKWARD:
+        if action == BACKWARD and self.current_position > 0:
             self.current_position -= 1
         #Forward
-        else:
+        elif action == FORWARD and self.current_position < len(self.current_track) - 1:
             self.current_position += 1
+        self.current_track[self.current_position] = self
+
+    #Reset to absolute position
+    def reset_to_position(self, position):
+        self.current_track[self.current_position] = None
+        self.current_position = position
         self.current_track[self.current_position] = self
 
     """
@@ -55,7 +62,7 @@ class Locomotive:
 class RailYardEnv(gym.Env):
     
     def __init__(self):
-        #actions are backwad and forward
+        #actions are backwad and forward       
         self.action_space = DiscreteDynamic(2)
         #states are all the possible positions of the locomotive
         self.observation_space = gym.spaces.Discrete(10)
@@ -64,7 +71,7 @@ class RailYardEnv(gym.Env):
         self.loco = Locomotive(self.track, 0)
 
     def reset(self):
-        self.loco.move(0)
+        self.loco.reset_to_position(0)
         self.action_space.disable_actions([BACKWARD])
 
     #move along the track until reach the end and update the available actions at each step
@@ -72,27 +79,27 @@ class RailYardEnv(gym.Env):
         self.loco.move(action)
         if self.loco.current_position == len(self.track) - 1:
             self.action_space.disable_actions([FORWARD])
-            reward = 20
+            reward = 1000
             done = True
         elif self.loco.current_position == 0:
             self.action_space.disable_actions([BACKWARD])
-            reward = -1
+            reward = -10
             done = False
         else:
             self.action_space.enable_actions([BACKWARD,FORWARD])
-            reward = -1
+            reward = -10
             done = False
         #print(self.track)
         return self.loco.current_position, reward , done, None
 
     def render(self,mode="human"):
-        out = ""
+        out = []
         for t in self.track:
             if t == None:
-                out += "0"
+                out.append(0)
             else:
-                out += "1"
-        sys.stdout.write(out + "\n")
+                out.append(1)
+        #sys.stdout.write(str(out) + "\n")
         return out
 
 class DiscreteDynamic(gym.spaces.Discrete):
@@ -137,9 +144,9 @@ def discount_rewards(r, gamma):
 print("Starting train simulator")
 
 #initialize tensors
-observations = tf.placeholder(dtype=tf.int32, shape=[None,10])
-actions = tf.placeholder(dtype=tf.int32,shape=[None])
-rewards = tf.placeholder(dtype=tf.int32,shape=[None])
+observations = tf.placeholder(dtype=tf.float32, shape=[None,10])
+actions = tf.placeholder(dtype=tf.int64,shape=[None])
+rewards = tf.placeholder(dtype=tf.float32,shape=[None])
 
 #setup model
 Y = tf.layers.dense(observations,10,activation=tf.nn.relu)
@@ -147,7 +154,9 @@ Ylogits = tf.layers.dense(Y,2)
 sample_op = tf.multinomial(logits=tf.reshape(Ylogits,shape=(1,2)), num_samples=1)
 
 #loss function
-cross_entropies = tf.losses.softmax_cross_entropy(onehot_labels=tf.one_hot(actions,(1,2)),logits=Ylogits)
+#print(Ylogits.get_shape())
+#print(tf.one_hot(actions,2))
+cross_entropies = tf.losses.softmax_cross_entropy(onehot_labels=tf.one_hot(actions,2),logits=Ylogits)
 loss = tf.reduce_sum(rewards * cross_entropies)
 
 #training operation setup
@@ -156,32 +165,51 @@ train_op = optimizer.minimize(loss)
 
 #trainimg iteration
 with tf.Session() as sess:
-    while len(observations) < BATCH_SIZE:
-        #rail yard environment set up
-        env = RailYardEnv()
-        env.reset()
-        counter = 0
-        reward = 0
-        done = False
-        while not done:
-            observation = env.render()
-            #decide move tp play
-            action = sess.run(sample_op, feed_dict={observations: [observation]})
-            #state, step_reward, done, info = env.step(env.action_space.sample())
-            state, step_reward, done, info = env.step(action)
-            counter += 1
-            reward += step_reward
-            #collect results
-            observations.append(observation)
-            actions.append(action)
-            rewards.append(reward)
+
+    #initialize variables and game experience memory
+    sess.run(tf.global_variables_initializer())
+    avg_reward = [0]*NUM_EPOCHS
+    
+    for epoch in range(NUM_EPOCHS):
+
+        print('\nStarting epoch ' + str(epoch) + '\n')
+        epoch_memory = []
+        episode_memory = []
+
+        for episode in range(NUM_EPISODES):
+
+            #print('\nStarting episode ' + str(episode) + '\n')
+            #rail yard environment set up
+            env = RailYardEnv()
+            env.reset()
+            counter = 0
+            reward = 0
+            done = False
+
+            #run the game
+            while not done:
+                observation = env.render()
+                #decide move tp play
+                action = sess.run(sample_op, feed_dict={observations: [observation]})
+                #state, step_reward, done, info = env.step(env.action_space.sample())
+                state, step_reward, done, info = env.step(action)
+                avg_reward[epoch] += step_reward
+                #collect results
+                episode_memory.append((observation, action[0][0], step_reward))
         
-        #discount rewards
-        processed_rewards = discount_rewards(rewards,0.99)
+            #discount rewards
+            episode_observations, episode_actions, episode_rewards = zip(*episode_memory)
+            processed_rewards = discount_rewards(episode_rewards,0.99)
+
+            #append to memory
+            epoch_memory.extend(zip(episode_observations, episode_actions, processed_rewards))
 
         #training step
-        feed_dict = {observations: observations, actions: action, rewards: processed_rewards}
+        epoch_observations, epoch_actions, epoch_rewards = zip(*epoch_memory)
+        feed_dict = {observations: epoch_observations, actions: epoch_actions, rewards: epoch_rewards}
         sess.run(train_op, feed_dict=feed_dict)
+        avg_reward[epoch] /= NUM_EPISODES
 
-        print(reward, " ", counter)
-                   
+    #print stats
+    for epoch in range(NUM_EPOCHS):
+        print("Epoch:" + str(epoch) + " avg reward: " + str(avg_reward[epoch]))
