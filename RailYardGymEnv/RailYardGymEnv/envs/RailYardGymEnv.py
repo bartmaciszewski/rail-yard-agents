2e783f468e094771553fef883494685e4ed5ee45
Ubuntu <bart@DSVMDEV01.h50fib20gemuxdhbr4qsur0oha.xx.internal.cloudapp.net>
2019-12-23 00:53:59 +0000

    refactoring railyard domain classes

--- RailYardGymEnv/RailYardGymEnv/envs/RailYardGymEnv.py
+++ RailYardGymEnv/RailYardGymEnv/envs/RailYardGymEnv.py
@@ -23,8 +23,11 @@ class RailYardGymEnv(gym.Env):
         
     def __init__(self):
         
+        #create the rail yard objects
+        self.ry = railyard.RailYard()
+
         #number of actions is defined by how many combinations of cars we can move from track to track and do nothing action     
-        self.action_space = DiscreteDynamic(NUMBER_OF_TRACKS*NUMBER_OF_TRACKS*NUMBER_OF_CARS+1)
+        self.action_space = DiscreteDynamic(ry.NUMBER_OF_TRACKS*ry.NUMBER_OF_TRACKS*NUMBER_OF_CARS+1)
         
         #state is the location and state of each rail car
         self.observation_space = RailCarBoxSpace()
@@ -322,159 +325,6 @@ class RailCarBoxSpace(gym.spaces.Box):
 
         return np.array(observation)
 
-
-class RailCar:
-    def __init__(self, ID, number, empty_full, product):
-        self.number = number
-        self.ID = ID
-        self.empty_or_full = empty_full
-        self.product = product
-
-    def is_empty(self):
-        return True if self.empty_or_full == EMPTY else False
-
-    def __str__(self):
-        if self.empty_or_full == EMPTY:
-            return self.number.lower()
-        else:
-            return self.number.capitalize()
-
-class Track:
-    def __init__(self, ID, length):
-        self.ID = ID
-        self.cars = []
-        self.length = length
-        self.connected_tracks = []
-
-    def push(self,car):
-        self.cars.append(car)
-
-    def pop(self):
-        return self.cars.pop()
-
-    def peek(self):
-        return self.cars[-1]
-
-    def is_empty(self):
-        return self.cars == []
-
-    def is_full(self):
-        return True if self.number_of_cars() == self.length else False
-
-    def number_of_cars(self):
-        return len(self.cars)
-
-    def get_cars(self):
-        return self.cars
-
-    def number_of_empty_spots(self):
-        return self.length - self.number_of_cars()
-
-    def derail_up(self):
-        return False
-
-    def __str__(self):
-        return str(self.ID) + ": " + str([str(car) for car in self.cars])
-
-    def connect(self, track):
-        self.connected_tracks.append(track)
-        track.connected_tracks.append(self)
-
-"""
-"NOT USED"
-#A switch is a track of size 1, track C diverges from the A and B straight ahead
-class Switch(Track):
-    def __init__(self, track_A, track_B, track_C):
-        self.track_A = track_A
-        self.track_B = track_B
-        self.track_C = track_C
-        self.A_B = SWITCH_POS_A
-        super(self.__class__, self).__init__(1)
-    
-    #throw the switch to either A or B position
-    def switch(self, switch_pos):
-        self.A_B = switch_pos
-"""
-
-class MarshallingTrack(Track):
-    """A track that is designated for a specific product.
-    
-    Attributes:
-        ID: the unique inteer identifier of this track in the yard
-        length: the length of the track in car units
-        product the type of proudct for which  this track is intended 
-    """
-
-    def __init__(self, ID, length, product):
-        self.product = product
-        super(self.__class__, self).__init__(ID, length)
-
-class Rack(Track):
-    def __init__(self, ID, num_bays, product, load_time):
-        self.load_time = load_time
-        self.product = product
-        self.current_load_time = 0
-        self.is_loading = False
-        super(self.__class__, self).__init__(ID, num_bays)
-
-    def start_load(self):
-        """Starts the process of loading cars under a rack if at least one car is product compatible."""
-
-        for car in self.cars:
-            if car.product == self.product:
-                self.current_load_time = 0
-                self.is_loading = True
-                break
-
-    def load_step(self):
-        """Continues loading cars on rack. Finish loading cars if sufficient time has passed."""
-
-        if self.is_loading == True and self.current_load_time >= self.load_time:
-            #we are done loading
-            for car in self.cars:
-                #only fill up car for the rack’s product
-                if car.product == self.product:
-                    car.empty_or_full = FULL 
-            self.is_loading = False
-            self.current_load_time = 0
-
-        self.current_load_time += 1
-
-    def is_currently_loading(self):
-        return self.is_loading
-
-    def derail_up(self):
-        return self.is_loading
-
-class Locomotive:
-    """An engine that can pull cars from track to track
-    """
-    def __init__(self):
-        self.active = False
-
-    def move_cars(self, tracks, source_track, destination_track, num_cars):
-        self.path = self.shortest_path(len(tracks), tracks, source_track, destination_track, num_cars)
-        self.current_hop = 0
-        self.active = True
-
-    def shortest_path(self, max_hops, tracks, source_track, destination_track, num_cars):
-        if destination_track.ID == 1:
-            #destination is the lead track
-            return [(source_track.ID, destination_track.ID, num_cars)]
-        else:
-            #first hop is the source to lead then lead to destination
-            return [(source_track.ID, 1, num_cars), (1, destination_track.ID, num_cars)]    
-
-
-    def is_active(self):
-        return self.active
-    
-    def next_hop(self):
-        hop = self.path[self.current_hop]
-        self.current_hop += 1
-        if self.current_hop == len(self.path):
-            self.active = False
-        return hop
 
 class LoadingSchedule:
     """A list of tuples that represents the rail cars and products that need to be loaded for a given set on a given day
