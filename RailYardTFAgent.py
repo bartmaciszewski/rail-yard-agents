4eef6080be3cdbb3f8d4c297969b6924a884a06c
Ubuntu <bart@DSVMDEVBRAZILSOUTH02.tzph0gldmzfupilxyq0vc5gqnf.nx.internal.cloudapp.net>
2020-01-04 22:10:38 +0000

    optimal policy for min scenario and improved dqn

--- RailYardTFAgent.py
+++ RailYardTFAgent.py
@@ -9,20 +9,35 @@ import PIL.Image
 #import pyvirtualdisplay
 
 import tensorflow as tf
+from tensorflow import keras
 
 from tf_agents.agents.dqn import dqn_agent
-from tf_agents.drivers import dynamic_step_driver
+from tf_agents.drivers.dynamic_step_driver import DynamicStepDriver
 from tf_agents.environments import suite_gym
 from tf_agents.environments import tf_py_environment
 from tf_agents.eval import metric_utils
 from tf_agents.metrics import tf_metrics
 from tf_agents.networks import q_network
-from tf_agents.policies import random_tf_policy
+from tf_agents.policies.random_tf_policy import RandomTFPolicy
 from tf_agents.replay_buffers import tf_uniform_replay_buffer
 from tf_agents.trajectories import trajectory
 from tf_agents.utils import common
+from tf_agents.utils.common import function
+from tf_agents.eval.metric_utils import log_metrics
+import logging
 
 import RailYardGymEnv
+from min_scenario_policy import MinYardScenarioPolicy
+
+class ShowProgress:
+    def __init__(self, total):
+        self.counter = 0
+        self.total = total
+    def __call__(self, trajectory):
+        if not trajectory.is_boundary():
+            self.counter += 1
+        if self.counter % 100 == 0:
+            print("\r{}/{}".format(self.counter, self.total), end="")
 
 def compute_avg_return(environment, policy, num_episodes=10):
     """Computes the average return of a policy.
@@ -50,38 +65,6 @@ def compute_avg_return(environment, policy, num_episodes=10):
     avg_return = total_return / num_episodes
     return avg_return.numpy()[0]
 
-def collect_step(environment, policy, buffer):
-    """Adds data to the buffer from a single state transition according to the policy.
-    
-    Args:
-        environment : a TF Python environment
-        policy : the policy to use to select an action for the step
-        buffer : the buffer to which to add the transition data
-    Returns:
-        None
-    """
-    time_step = environment.current_time_step()
-    action_step = policy.action(time_step)
-    next_time_step = environment.step(action_step.action)
-    traj = trajectory.from_transition(time_step, action_step, next_time_step)
-
-    #Add trajectory to the replay buffer
-    buffer.add_batch(traj)
-
-def collect_data(env, policy, buffer, steps):
-    """Collects in a buffer results of executing a policy for some number of steps.
-    
-    Args:
-        env : a TF Python environment
-        policy : the policy to use to select actions
-        buffer : the buffer in which to store the results
-        steps : the number of steps to execute
-    Returns:
-        None
-    """
-    for _ in range(steps):
-        collect_step(env, policy, buffer)
-
 def observation_and_action_constraint_splitter(observation):
     """Used by DQN agent to constrain the action space based on state. 
     Uses the current_py_env global variable to get the constrained action space
@@ -108,7 +91,6 @@ def create_policy_eval_text_log(policy, filename, num_episodes=5):
             logfile.write("Action: " + str(eval_py_env.decode_action(action_step.action.numpy()[0])) + "\n")
             logfile.write(eval_py_env.render())
 
-
 def create_policy_eval_video(policy, filename, num_episodes=5, fps=30):
     filename = filename + ".mp4"
     with imageio.get_writer(filename, fps=fps) as video:
@@ -120,19 +102,31 @@ def create_policy_eval_video(policy, filename, num_episodes=5, fps=30):
                 time_step = eval_env.step(action_step.action)
                 video.append_data(eval_py_env.render())
 
+def train_agent(n_iterations):
+    time_step = None
+    policy_state = agent.collect_policy.get_initial_state(train_env.batch_size)
+    iterator = iter(dataset)
+    for iteration in range(n_iterations):
+        time_step, policy_state = collect_driver.run(time_step, policy_state)
+        trajectories, buffer_info = next(iterator)
+        train_loss = agent.train(trajectories)
+        print("\r{} loss:{:.5f}".format(
+            iteration, train_loss.loss.numpy()), end="")
+        if iteration % 1000 == 0:
+            log_metrics(train_metrics)
+
 tf.compat.v1.enable_v2_behavior()
 
 #training parameters
-num_iterations = 10 #number of training iterations (e.g. play a number of steps and then train) 
-collect_steps_per_iteration = 1000 #how many steps to play in each training iteration
-replay_buffer_max_length = 10000
-batch_size = 1000
-learning_rate = 1e-3
+num_iterations = 1000000 #number of training iterations (e.g. play a number of steps and then train) 
+collect_steps_per_iteration = 10 #how many steps to play in each training iteration
+replay_buffer_max_length = 1000000
+batch_size = 32
+learning_rate = 1e-2
 log_interval = 2 
 
 #how many episodes to play to evaluate the agent 
 num_eval_episodes = 5 
-eval_interval = 2
 
 #create training and evaluation environment and convert to Tensorflow environments
 env_name = 'RailYardGymEnv-v0'
@@ -145,23 +139,12 @@ eval_env.reset()
 
 #py environment used by DQN agent's observation_action_splitter when choosing next action based on state
 current_py_env = train_py_env
-'''
-#Test spaces
-print('Observation Spec:')
-print(train_env.time_step_spec().observation)
-
-print('Reward Spec:')
-print(train_env.time_step_spec().reward)
-
-print('Action Spec:')
-print(train_env.action_spec())
-'''
 
 #Build the Qnetwork
 #TODO(bmac): normalize the input to the network
 #preprocessing_layer = keras.layers.Lambda(
 #                          lambda obs: tf.cast(obs, np.float32) / 255.)
-fc_layer_params = (100,)
+fc_layer_params = [32,32]
 q_net = q_network.QNetwork(
     train_env.observation_spec(),
     train_env.action_spec(),
@@ -171,22 +154,40 @@ q_net = q_network.QNetwork(
     #        axis=-1),
     fc_layer_params=fc_layer_params)
 
-#print(train_env.observation_spec())
-#print('Observation Spec:')
-#print(train_env.time_step_spec().observation)
-
 #Instantiate the DQN agent
-optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate)
-train_step_counter = tf.Variable(0)
-agent = dqn_agent.DqnAgent(
-    train_env.time_step_spec(),
-    train_env.action_spec(),
-    q_network=q_net,
-    optimizer=optimizer,
-    observation_and_action_constraint_splitter=observation_and_action_constraint_splitter,
-    epsilon_greedy=0.8,
-    td_errors_loss_fn=common.element_wise_squared_loss,
-    train_step_counter=train_step_counter)
+#optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate)
+#train_step_counter = tf.Variable(0)
+#agent = dqn_agent.DqnAgent(
+#    train_env.time_step_spec(),
+#    train_env.action_spec(),
+#    q_network=q_net,
+#    optimizer=optimizer,
+#    observation_and_action_constraint_splitter=observation_and_action_constraint_splitter,
+#    epsilon_greedy=0.5,
+#    td_errors_loss_fn=common.element_wise_squared_loss,
+#    train_step_counter=train_step_counter)
+#agent.initialize()
+
+from tf_agents.agents.dqn.dqn_agent import DqnAgent
+
+train_step = tf.Variable(0)
+update_period = 10 # run a training step every 10 collect steps
+optimizer = tf.compat.v1.train.RMSPropOptimizer(learning_rate=2.5e-4, decay=0.95, momentum=0.0,
+                                     epsilon=0.00001, centered=True)
+epsilon_fn = keras.optimizers.schedules.PolynomialDecay(
+    initial_learning_rate=1.0, # initial ε
+    decay_steps=num_iterations,
+    end_learning_rate=0.01) # final ε
+agent = DqnAgent(train_env.time_step_spec(),
+                 train_env.action_spec(),
+                 q_network=q_net,
+                 optimizer=optimizer,
+                 observation_and_action_constraint_splitter=observation_and_action_constraint_splitter,
+                 target_update_period=10,
+                 td_errors_loss_fn=keras.losses.Huber(reduction="none"),
+                 gamma=0.99, # discount factor
+                 train_step_counter=train_step,
+                 epsilon_greedy=lambda: epsilon_fn(train_step))
 agent.initialize()
 
 # A buffer to stores the experience from interacting with the environment
@@ -195,57 +196,53 @@ replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
     batch_size=train_env.batch_size,
     max_length=replay_buffer_max_length)
 
+#An observer to write the trajectories to the buffer
+replay_buffer_observer = replay_buffer.add_batch
+
+#Training metrics
+train_metrics = [
+    tf_metrics.NumberOfEpisodes(),
+    tf_metrics.EnvironmentSteps(),
+    tf_metrics.AverageReturnMetric(),
+    tf_metrics.AverageEpisodeLengthMetric()
+]
+logging.getLogger().setLevel(logging.INFO)
+
+#Create the driver
+collect_driver = DynamicStepDriver(
+    train_env,
+    #agent.collect_policy,
+    MinYardScenarioPolicy(train_env.time_step_spec(),train_env.action_spec()),
+    observers=[replay_buffer_observer] + train_metrics,
+    num_steps=collect_steps_per_iteration) # collect # steps for each training iteration
+
+#Collect some inital experience with random policy
+initial_collect_policy = RandomTFPolicy(train_env.time_step_spec(),
+                                        train_env.action_spec())
+init_driver = DynamicStepDriver(
+    train_env,
+    initial_collect_policy,
+    observers=[replay_buffer.add_batch,ShowProgress(1000)],
+    num_steps=1000)
+#final_time_step, final_policy_state = init_driver.run()
+
 # Dataset generates trajectories with shape [Bx2x...]
 dataset = replay_buffer.as_dataset(
     num_parallel_calls=3, 
     sample_batch_size=batch_size, 
     num_steps=2).prefetch(3)
 
-iterator = iter(dataset)
-
-# (Optional) Optimize by wrapping some of the code in a graph using TF function.
-agent.train = common.function(agent.train)
-
-# Reset the train step
-agent.train_step_counter.assign(0)
+collect_driver.run = function(collect_driver.run)
+agent.train = function(agent.train)
 
 # Evaluate the agent's policy once before training.
-current_py_env = eval_py_env
-avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
-returns = [avg_return]
+#current_py_env = eval_py_env
+#avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
+#returns = [avg_return]
 
 current_py_env = train_py_env
-
-for _ in range(num_iterations):
-
-  # Collect a few steps using collect_policy and save to the replay buffer.
-  for _ in range(collect_steps_per_iteration):
-    collect_step(train_env, agent.collect_policy, replay_buffer)
-    #if train_env.current_time_step().is_last():
-    #    train_env.reset()
-
-  # Sample a batch of data from the buffer and update the agent's network.
-  experience, unused_info = next(iterator)
-  train_loss = agent.train(experience).loss
-
-  step = agent.train_step_counter.numpy()
-
-  if step % log_interval == 0:
-    print('step = {0}: loss = {1}'.format(step, train_loss))
-
-  if step % eval_interval == 0:
-    current_py_env = eval_py_env
-    avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
-    print('step = {0}: Average Return = {1}'.format(step, avg_return))
-    returns.append(avg_return)
-    current_py_env = train_py_env
-
-iterations = range(0, num_iterations + 1, eval_interval)
-plt.plot(iterations, returns)
-plt.ylabel('Average Return')
-plt.xlabel('Iterations')
-plt.ylim(top=1000,bottom=-10000)
+train_agent(num_iterations)
 
 current_py_env = eval_py_env
-create_policy_eval_text_log(agent.policy, "trained-agent")
+create_policy_eval_text_log(agent.policy, "trained-agent",num_eval_episodes)
 #create_policy_eval_video(agent.policy, "trained-agent")
