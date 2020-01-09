9fe5af33028382bd1d7d5c9127153b8d8eb279ed
Ubuntu <bart@DSVMDEVBRAZILSOUTH02.tzph0gldmzfupilxyq0vc5gqnf.nx.internal.cloudapp.net>
2020-01-09 00:23:04 +0000

    generalized to support many cars

--- railyard_q_learning_agent.py
+++ railyard_q_learning_agent.py
@@ -6,8 +6,7 @@ def exploration_policy(env):
     return np.random.choice(env.possible_actions())
 
 def observation_to_str(state):
-    #TODO: generalize for more than one line in observation 
-    return "".join(str(e) for e in state[0])
+    return "".join(str(c) for l in state for c in l)
 
 #hyperparamerts
 num_iterations = 1000
@@ -50,6 +49,9 @@ for i in range(num_iterations):
     
     #update the Q value based on the reward we got
     alpha = alpha0 / (1 + i * decay)
+    print(current_state_str)
+    print(action_index)
+    print(Q_values[current_state_str])
     Q_values[current_state_str][action_index] *= (1 - alpha)
     Q_values[current_state_str][action_index] += alpha*(next_state.reward + gamma*next_value)
     
@@ -58,8 +60,8 @@ for i in range(num_iterations):
     
     current_state = train_py_env.current_time_step()
 
-    print("\r Iteration:{} Q value max{}".format(
-        i, np.max(Q_values[initial_state_str]), end=""))
+    print("\r Iteration:{} Q value max: {} # states: {}".format(
+        i, np.max(Q_values[initial_state_str]), len(Q_values), end=""))
     #print(Q_values)
 
 print(Q_values)
