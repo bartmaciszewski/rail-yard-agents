36daf5a527944056a1d51107d86ba008c2f993fa
bmaci <11906299+bmaciszewski@users.noreply.github.com>
2020-02-19 05:09:49 +0000

    Revert "Pin demo"

--- RailYardGymEnv/RailYardGymEnv/__init__.py
+++ RailYardGymEnv/RailYardGymEnv/__init__.py
@@ -2,12 +2,6 @@ from gym.envs.registration import register
 
 register(
     id='RailYardGymEnv-v0',
-    max_episode_steps=100,
+    max_episode_steps=10000,
     entry_point='RailYardGymEnv.envs.railyard_gym_env:RailYardGymEnv',
-)
-
-register(
-    id='MinScenarioRailYardGymEnv-v0',
-    max_episode_steps=100,
-    entry_point='RailYardGymEnv.envs.railyard_gym_env:MinScenarioRailYardGymEnv',
 )
